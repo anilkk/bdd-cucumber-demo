14e3f2adacec9ba3e6b7104d4d6462a24cbfd241
Anil Kumar krishanshetty <anilbms75@gmail.com>
2020-04-20 23:37:38 +0200

    Update slide changes

--- src/App.js
+++ src/App.js
@@ -1,72 +1,25 @@
 import React from "react";
-import Brightness3Icon from "@material-ui/icons/Brightness3";
-import WbSunnyIcon from "@material-ui/icons/WbSunny";
-import WbIncandescentIcon from "@material-ui/icons/WbIncandescent";
-import EmojiObjectsTwoToneIcon from "@material-ui/icons/EmojiObjectsTwoTone";
 import Grid from "@material-ui/core/Grid";
-import ToggleButton from "@material-ui/lab/ToggleButton";
-import ToggleButtonGroup from "@material-ui/lab/ToggleButtonGroup";
 import Alert from "@material-ui/lab/Alert";
+import AlertTitle from "@material-ui/lab/AlertTitle";
 import "./styles.css";
 
-const outcomes = {
-  daylighton: {
-    url:
-      "https://s3-us-west-2.amazonaws.com/figma-alpha-api/img/c0b0/892a/67b9a888d92879384fa9282a8e7fd4c3",
-    context: "day with light on",
-    state: "can read the news paper"
-  },
-  daylightoff: {
-    url:
-      "https://s3-us-west-2.amazonaws.com/figma-alpha-api/img/c4d4/56ef/24154f97bf9478debe59aaf92dd1e052",
-    context: "day with light off",
-    state: "can read the news paper"
-  },
-  nightlighton: {
-    url:
-      "https://s3-us-west-2.amazonaws.com/figma-alpha-api/img/4238/e3a4/ac35fda338f0116c0f5779b6c3af068a",
-    context: "night with light on",
-    state: "can read the news paper"
-  },
-  nightlightoff: {
-    url:
-      "https://s3-us-west-2.amazonaws.com/figma-alpha-api/img/dacc/8201/0044f49c389a2243f91d97eb907e995a",
-    context: "night with light off",
-    state: "can't read the news paper"
-  }
-};
+const bulbLightOn =
+  "https://s3-us-west-2.amazonaws.com/figma-alpha-api/img/2b55/060a/006b8e70c722b42723d29228d9321e3e";
+const bulbLightOff =
+  "https://s3-us-west-2.amazonaws.com/figma-alpha-api/img/a828/79de/6460848b8c2e11c8340911803fb7c8fb";
+const sunAndCLoudUrl =
+  "https://s3-us-west-2.amazonaws.com/figma-alpha-api/img/6633/90f1/1d2d41a10dc07ee4cad3e9988a24f10e";
+const moonAndStarUrl =
+  "https://s3-us-west-2.amazonaws.com/figma-alpha-api/img/1e73/5a4c/03f3716dbf829d9f173283674258196e";
+const homeWithLightUrl =
+  "https://s3-us-west-2.amazonaws.com/figma-alpha-api/img/646b/60ba/9bd59b5d1072b939bc58f6408bb5c4c6";
+const homeWithoutLightUrl =
+  "https://s3-us-west-2.amazonaws.com/figma-alpha-api/img/0bf4/347c/88d5c0c68d76f3b8026386981eb1a0a3";
+
 const ToggleButtonSizes = function() {
-  const [time, setTime] = React.useState("day");
-  const [light, setLight] = React.useState("off");
-
-  const handleChangeTime = (event, newTime) => {
-    setTime(newTime || "day");
-  };
-
-  const handleChangeLight = (event, newLight) => {
-    setLight(newLight || "off");
-  };
-
-  const children = [
-    <ToggleButton key={1} value="day" test-data="day">
-      Day <WbSunnyIcon style={time === "day" ? { color: "orange" } : {}} />
-    </ToggleButton>,
-    <ToggleButton key={2} value="night" test-data="night">
-      Night
-      <Brightness3Icon style={time === "night" ? { color: "fff" } : {}} />
-    </ToggleButton>
-  ];
-
-  const lightChildren = [
-    <ToggleButton key={1} value="on" test-data="light-on">
-      On{" "}
-      <WbIncandescentIcon style={light === "on" ? { color: "#cc6d2e" } : {}} />
-    </ToggleButton>,
-    <ToggleButton key={2} value="off" test-data="light-off">
-      off
-      <EmojiObjectsTwoToneIcon />
-    </ToggleButton>
-  ];
+  const [myLight, setMyLight] = React.useState(false);
+  const [myTime, setMyTime] = React.useState(true);
 
   return (
     <Grid
@@ -74,76 +27,83 @@ const ToggleButtonSizes = function() {
       direction="row"
       alignItems="center"
       spacing={3}
-      className={time}
+      className={myTime ? "day" : "night"}
     >
-      <Grid xs={12}>
-        <h1>BDD Cucumber demo</h1>
-        <hr />
-      </Grid>
-      <Grid item xs={12} sm={4}>
+      <Grid sm={12} />
+      <Grid item sm={4}>
         <Grid item>
-          <h3>Context (GIVEN)</h3>
-          <ToggleButtonGroup
-            size="small"
-            value={time}
-            exclusive
-            onChange={handleChangeTime}
-          >
-            {children}
-          </ToggleButtonGroup>
-          <p>
-            John reading the news papper in the{" "}
-            {outcomes[time + "light" + light].context}
+          <h1 className="banner-header">BDD Demo</h1>
+        </Grid>
+        <Grid item>
+          <h3>
+            Change Context (GIVEN):{" "}
+            <label className="switch">
+              <input
+                type="checkbox"
+                checked={myTime}
+                onChange={event => {
+                  setMyTime(event.target.checked);
+                }}
+              />
+              <span class="slider round" />
+            </label>{" "}
+            {/* Day */}
+          </h3>
+          <img
+            src={myTime ? sunAndCLoudUrl : moonAndStarUrl}
+            alt={myTime ? "Sun and cloud" : "moon and star"}
+          />
+          <p className="font-sty-italic">
+            Maria reading the news papper in the {myTime} with light{" "}
+            {myLight ? "on" : "off"}
           </p>
           <hr />
         </Grid>
-        <Grid item>
-          <h3>Action (WHEN)</h3>
-          <ToggleButtonGroup
-            size="small"
-            value={light}
-            exclusive
-            onChange={handleChangeLight}
-          >
-            {lightChildren}
-          </ToggleButtonGroup>
-          <p>John can perform this action</p>
-          <hr />
-        </Grid>
-        <Grid>
-          <h3>Outcome (THEN)</h3>
-          <h3>
-            {!(time === "night" && light === "off") && (
-              <Alert variant="filled" severity="success" test-data="success">
-                John {outcomes[time + "light" + light].state}
-              </Alert>
-            )}
-            {time === "night" && light === "off" && (
-              <Alert variant="filled" severity="error" test-data="fail">
-                John {outcomes[time + "light" + light].state}
-              </Alert>
-            )}
-          </h3>
+        <Grid item container direction="column" alignItems="flex-end">
+          <Grid item>
+            <h3>
+              Trigger Action (WHEN):{" "}
+              <label className="switch">
+                <input
+                  type="checkbox"
+                  checked={myLight}
+                  onChange={event => {
+                    setMyLight(event.target.checked);
+                  }}
+                />
+                <span class="slider round" />
+              </label>
+            </h3>
+          </Grid>
+          <Grid item>
+            <img
+              src={myLight ? bulbLightOn : bulbLightOff}
+              alt={myLight ? "Bulb light on" : "Bulb light off"}
+            />
+            <p className="font-sty-italic">Maria can perform this action</p>
+          </Grid>
         </Grid>
       </Grid>
-      <Grid item xs={12} sm={8}>
-        <h3>
-          {!(time === "night" && light === "off") && (
-            <Alert variant="filled" severity="success" test-data="success">
-              John {outcomes[time + "light" + light].state}
-            </Alert>
-          )}
-          {time === "night" && light === "off" && (
-            <Alert variant="filled" severity="error" test-data="fail">
-              John {outcomes[time + "light" + light].state}
-            </Alert>
-          )}
-        </h3>
+      <Grid item sm={8}>
         <img
           style={{ "max-width": "100%" }}
-          src={outcomes[time + "light" + light].url}
-          alt={outcomes[time + "light" + light].context}
+          src={myLight ? homeWithLightUrl : homeWithoutLightUrl}
+          alt="home"
         />
+        <Grid container justify="center">
+          <h2>
+            <Alert
+              variant="filled"
+              icon={false}
+              severity={myTime || myLight ? "success" : "error"}
+              test-data={myTime || myLight ? "success" : "fail"}
+            >
+              <AlertTitle>Outcome (THEN):</AlertTitle>
+              Maria {myTime || myLight ? "can" : "can't"}{" "}
+              <strong>read the book</strong>{" "}
+            </Alert>
+          </h2>
+        </Grid>
       </Grid>
     </Grid>
   );
